a8bbb9b7b6e25a98afdf9e10c61569538c16ac11
Henrique Evangelista <henriqueevangelistapimentel@gmail.com>
2026-07-29 17:39:32 -0300

    Criação da rota /produtos e integração ao servidor

--- serves.js
+++ serves.js
@@ -1,8 +1,23 @@
 const express = require('express');
+
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
+
+// Puxando a conexão com o banco de dados
+require('./src/config/db');
+
 
 // Permite que o servidor entenda dados enviados no formato JSON
 app.use(express.json());
+
+// Importando o arquivo de rotas que acabamos de criar
+const produtoRoutes = require('./src/routes/produtoRoutes');
+
+// Avisando ao servidor para usar essas rotas
+app.use('/', produtoRoutes);
 
 // Rota de teste simples para verificar se o servidor está funcionando
 app.get('/', (req, res) => {
